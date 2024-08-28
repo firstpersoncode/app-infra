69f84c4079fd612cb5b4590b3a743aebc253173e
firstpersoncode <fpcdatadriver@gmail.com>
2024-08-29 04:21:34 +0800

    Revert "update"
    This reverts commit b0c9fcb30eb6421bccf581344a1553e9041444dd.

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return `BUILD_ENV ${process.env.BUILD_ENV}`;
+    return 'Hello World!';
   }
 }
