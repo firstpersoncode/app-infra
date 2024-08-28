78ffec9f78449ccafc2ae93ad52a7fd7b60da22a
firstpersoncode <fpcdatadriver@gmail.com>
2024-08-28 23:53:24 +0800

    update

--- infra/remote_backend_s3.tf
+++ infra/remote_backend_s3.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "dev-proj-1-app-remote-state-bucket-66996699"
+    bucket = "dev-proj-1-app-infra-remote-state-bucket-66996699"
     key    = "devops-project-1/terraform.tfstate"
     region = "ap-southeast-2"
   }
